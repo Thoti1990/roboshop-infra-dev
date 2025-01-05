502fe58c19c6756f6ceb63866af03a95a1698c38
Thoti1990 <thotinagaraju437@gmail.com>
2025-01-05 13:42:07 +0530

    terraform

--- 02-sg/main.tf
+++ 02-sg/main.tf
@@ -118,88 +118,118 @@ module "web_alb" {
 }
 
 
-# App alb should accept connections only from vpn, since it is internal
+# App ALB should accept connections only from VPN, since it is internal
 resource "aws_security_group_rule" "app_alb_vpn" {
   source_security_group_id = module.vpn.sg_id
-  type              = "ingress"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  security_group_id = module.app_alb.sg_id
+  type                     = "ingress"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+  security_group_id        = module.app_alb.sg_id
 }
 
 resource "aws_security_group_rule" "app_alb_web" {
   source_security_group_id = module.web.sg_id
-  type              = "ingress"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  security_group_id = module.app_alb.sg_id
+  type                     = "ingress"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+  security_group_id        = module.app_alb.sg_id
 }
+
+resource "aws_security_group_rule" "app_alb_cart" {
+  source_security_group_id = module.cart.sg_id
+  type                     = "ingress"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+  security_group_id        = module.app_alb.sg_id
+}
+
+resource "aws_security_group_rule" "app_alb_shipping" {
+  source_security_group_id = module.shipping.sg_id
+  type                     = "ingress"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+  security_group_id        = module.app_alb.sg_id
+}
+
+resource "aws_security_group_rule" "app_alb_user" {
+  source_security_group_id = module.user.sg_id
+  type                     = "ingress"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+  security_group_id        = module.app_alb.sg_id
+}
+
+resource "aws_security_group_rule" "app_alb_catalogue" {
+  source_security_group_id = module.catalogue.sg_id
+  type                     = "ingress"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+  security_group_id        = module.app_alb.sg_id
+}
+
+resource "aws_security_group_rule" "app_alb_payment" {
+  source_security_group_id = module.payment.sg_id
+  type                     = "ingress"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+  security_group_id        = module.app_alb.sg_id
+}
+
+
 
 resource "aws_security_group_rule" "web_alb_internet" {
   cidr_blocks = ["0.0.0.0/0"]
-  type              = "ingress"
-  from_port         = 443
-  to_port           = 443
-  protocol          = "tcp"
-  security_group_id = module.web_alb.sg_id
+  type                     = "ingress"
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
+  security_group_id        = module.web_alb.sg_id
 }
 
 #openvpn
 resource "aws_security_group_rule" "vpn_home" {
   security_group_id = module.vpn.sg_id
-  type              = "ingress"
-  from_port         = 0
-  to_port           = 65535
-  protocol          = "-1"
-  cidr_blocks = ["0.0.0.0/0"] #ideally your home public ip address, but it frequently changes   
+  type                     = "ingress"
+  from_port                = 0
+  to_port                  = 65535
+  protocol                 = "-1"
+  cidr_blocks = ["0.0.0.0/0"] #ideally your home public IP address, but it frequently changes
 }
+
 
 resource "aws_security_group_rule" "mongodb_vpn" {
   source_security_group_id = module.vpn.sg_id
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  security_group_id = module.mongodb.sg_id
+  type                     = "ingress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  security_group_id        = module.mongodb.sg_id
 }
 
 #mongodb accepting connections from catalogue instance
 resource "aws_security_group_rule" "mongodb_catalogue" {
   source_security_group_id = module.catalogue.sg_id
-  type              = "ingress"
-  from_port         = 27017
-  to_port           = 27017
-  protocol          = "tcp"
-  security_group_id = module.mongodb.sg_id
+  type                     = "ingress"
+  from_port                = 27017
+  to_port                  = 27017
+  protocol                 = "tcp"
+  security_group_id        = module.mongodb.sg_id
 }
 
 resource "aws_security_group_rule" "mongodb_user" {
   source_security_group_id = module.user.sg_id
-  type              = "ingress"
-  from_port         = 27017
-  to_port           = 27017
-  protocol          = "tcp"
-  security_group_id = module.mongodb.sg_id
-}
-
-resource "aws_security_group_rule" "redis_user" {
-  source_security_group_id = module.user.sg_id
-  type              = "ingress"
-  from_port         = 6379
-  to_port           = 6379
-  protocol          = "tcp"
-  security_group_id = module.redis.sg_id
-}
-
-resource "aws_security_group_rule" "redis_cart" {
-  source_security_group_id = module.cart.sg_id
-  type              = "ingress"
-  from_port         = 6379
-  to_port           = 6379
-  protocol          = "tcp"
-  security_group_id = module.redis.sg_id
+  type                     = "ingress"
+  from_port                = 27017
+  to_port                  = 27017
+  protocol                 = "tcp"
+  security_group_id        = module.mongodb.sg_id
 }
 
 resource "aws_security_group_rule" "redis_vpn" {
@@ -210,6 +240,25 @@ resource "aws_security_group_rule" "redis_vpn" {
   protocol                 = "tcp"
   security_group_id        = module.redis.sg_id
 }
+
+resource "aws_security_group_rule" "redis_user" {
+  source_security_group_id = module.user.sg_id
+  type                     = "ingress"
+  from_port                = 6379
+  to_port                  = 6379
+  protocol                 = "tcp"
+  security_group_id        = module.redis.sg_id
+}
+
+resource "aws_security_group_rule" "redis_cart" {
+  source_security_group_id = module.cart.sg_id
+  type                     = "ingress"
+  from_port                = 6379
+  to_port                  = 6379
+  protocol                 = "tcp"
+  security_group_id        = module.redis.sg_id
+}
+
 
 resource "aws_security_group_rule" "mysql_vpn" {
   source_security_group_id = module.vpn.sg_id
@@ -253,6 +302,15 @@ resource "aws_security_group_rule" "catalogue_vpn" {
   type                     = "ingress"
   from_port                = 22
   to_port                  = 22
+  protocol                 = "tcp"
+  security_group_id        = module.catalogue.sg_id
+}
+
+resource "aws_security_group_rule" "catalogue_vpn_http" {
+  source_security_group_id = module.vpn.sg_id
+  type                     = "ingress"
+  from_port                = 8080
+  to_port                  = 8080
   protocol                 = "tcp"
   security_group_id        = module.catalogue.sg_id
 }
@@ -338,23 +396,32 @@ resource "aws_security_group_rule" "cart_vpn" {
 #   security_group_id        = module.cart.sg_id
 # }
 
-# resource "aws_security_group_rule" "cart_shipping" {
-#   source_security_group_id = module.shipping.sg_id
-#   type                     = "ingress"
-#   from_port                = 8080
-#   to_port                  = 8080
-#   protocol                 = "tcp"
-#   security_group_id        = module.cart.sg_id
-# }
+resource "aws_security_group_rule" "cart_app_alb" {
+  source_security_group_id = module.app_alb.sg_id
+  type                     = "ingress"
+  from_port                = 8080
+  to_port                  = 8080
+  protocol                 = "tcp"
+  security_group_id        = module.cart.sg_id
+}
 
-# resource "aws_security_group_rule" "cart_payment" {
-#   source_security_group_id = module.payment.sg_id
-#   type                     = "ingress"
-#   from_port                = 8080
-#   to_port                  = 8080
-#   protocol                 = "tcp"
-#   security_group_id        = module.cart.sg_id
-# }
+resource "aws_security_group_rule" "cart_shipping" {
+  source_security_group_id = module.shipping.sg_id
+  type                     = "ingress"
+  from_port                = 8080
+  to_port                  = 8080
+  protocol                 = "tcp"
+  security_group_id        = module.cart.sg_id
+}
+
+resource "aws_security_group_rule" "cart_payment" {
+  source_security_group_id = module.payment.sg_id
+  type                     = "ingress"
+  from_port                = 8080
+  to_port                  = 8080
+  protocol                 = "tcp"
+  security_group_id        = module.cart.sg_id
+}
 
 resource "aws_security_group_rule" "shipping_vpn" {
   source_security_group_id = module.vpn.sg_id
@@ -374,6 +441,15 @@ resource "aws_security_group_rule" "shipping_vpn" {
 #   security_group_id        = module.shipping.sg_id
 # }
 
+resource "aws_security_group_rule" "shipping_app_alb" {
+  source_security_group_id = module.app_alb.sg_id
+  type                     = "ingress"
+  from_port                = 8080
+  to_port                  = 8080
+  protocol                 = "tcp"
+  security_group_id        = module.shipping.sg_id
+}
+
 resource "aws_security_group_rule" "payment_vpn" {
   source_security_group_id = module.vpn.sg_id
   type                     = "ingress"
@@ -391,6 +467,15 @@ resource "aws_security_group_rule" "payment_vpn" {
 #   protocol                 = "tcp"
 #   security_group_id        = module.payment.sg_id
 # }
+
+resource "aws_security_group_rule" "payment_app_alb" {
+  source_security_group_id = module.app_alb.sg_id
+  type                     = "ingress"
+  from_port                = 8080
+  to_port                  = 8080
+  protocol                 = "tcp"
+  security_group_id        = module.payment.sg_id
+}
 
 resource "aws_security_group_rule" "web_vpn" {
   source_security_group_id = module.vpn.sg_id
---
+++ 13-shipping/variables.tf
@@ -0,0 +1,29 @@
+variable "common_tags" {
+  default = {
+    project     = "roboshop"
+    terraform   = "true"
+    environment = "dev"
+  }
+}
+
+variable "tags" {
+  default = {
+    component = "shipping"
+  }
+}
+
+variable "project_name" {
+  default = "roboshop"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "zone_name" {
+  default = "devopsaws.site"
+}
+
+variable "iam_instance_profile" {
+  default = "ec2fullaccesforshell"
+}
