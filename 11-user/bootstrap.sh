1dbf572f853f5ed3aadcb593b8947b0d7f07d047
Thoti1990 <thotinagaraju437@gmail.com>
2025-01-17 19:37:08 +0530

    commited

--- 11-user/bootstrap.sh
+++ 11-user/bootstrap.sh
@@ -4,4 +4,4 @@ component=$1
 environment=$2 #dont use env here, it is reserved in linux
 yum install python3.11-devel python3.11-pip -y
 pip3.11 install ansible botocore boto3
-ansible-pull -U https://github.com/Thoti1990/roboshop-ansible-roles-tf.git -e component=$component -e env=$environment main-tf.yaml
+ansible-pull -U https://github.com/Thoti1990/roboshop-ansible-roless-tf.git -e component=$component -e env=$environment main-tf.yaml
